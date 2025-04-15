f21189e98205a16e88f16ad2e4d829c83373f635
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-04-15 18:47:16 +0300

    add OrderStarted domain event and handler to integrate with Buyer context

---
+++ Order.Application/DomainEventHandlers/OrderStartedDomainEventHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Order.Application.Repository;
+using Order.Domain.AggregateModels.BuyerModels;
+using Order.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Application.DomainEventHandlers
+{
+    public class OrderStartedDomainEventHandler : INotificationHandler<OrderStartedDomainEvent>
+    {
+        private readonly IBuyerRepository buyerRepository;
+
+        public OrderStartedDomainEventHandler(IBuyerRepository buyerRepository)
+        {
+            this.buyerRepository = buyerRepository;
+        }
+
+        public Task Handle(OrderStartedDomainEvent notification, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(notification.Order.UserName))
+            {
+                var buyer = new Buyer(notification.Order.UserName);
+                
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
